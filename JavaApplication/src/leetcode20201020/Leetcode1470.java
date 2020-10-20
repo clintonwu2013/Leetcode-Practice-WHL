68bf5094b78b6b396fa1c3b35d321717c36fbd86
m2912 <m2912@Timo-wu.wanhai.com>
2020-10-20 15:45:42 +0800

    commit

---
+++ JavaApplication/src/leetcode20201020/Leetcode1470.java
@@ -0,0 +1,22 @@
+package leetcode20201020;
+
+public class Leetcode1470 {
+	public int[] shuffle(int[] nums, int n) {
+		int[] ans = new int[2*n];
+		int preIndex = 0;
+		int suIndex = n;
+		int idx = 0;
+		while(preIndex<n && suIndex < 2*n) {
+			ans[idx] = nums[preIndex];
+			ans[++idx] = nums[suIndex];
+			
+			preIndex++;
+			suIndex++;
+			idx++;
+			
+		}
+		
+		
+		return ans;
+	}
+}
