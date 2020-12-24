5e6106e9ed4abfe1ec006c6212008e7deb1ad6c9
吳宗霖 <clintonwu2013@gmail.com>
2020-12-25 00:13:49 +0800

    commit

---
+++ JavaApplication/src/leetcode20200527/Leetcode5.java
@@ -0,0 +1,35 @@
+package leetcode20200527;
+
+public class Leetcode5 {
+	public String longestPalindrome(String s) {
+		int start = 0, end = 0;
+		boolean[][] d = new boolean[s.length()][s.length()];
+		for (int i = 0; i < s.length(); i++) {
+			d[i][i] = true;
+		}
+
+		for (int i = 1; i < s.length(); i++) {
+			for (int j = 0; j < i; j++) {
+				if (s.charAt(i) == s.charAt(j)) {
+					if (i - j == 1 || d[i - 1][j + 1]) {
+						d[i][j] = true;
+						if (end - start < i - j) {
+							end = i;
+							start = j;
+						}
+					}
+
+				}
+			}
+		}
+
+		return s.substring(start, end + 1);
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ JavaApplication/src/leetcode20200527/Leetcode62.java
@@ -0,0 +1,25 @@
+package leetcode20200527;
+
+public class Leetcode62 {
+	
+	public int uniquePaths(int m, int n) {
+		
+		if(m<=0 || n<=0) {
+			return 0;
+		}
+		int[][] s = new int[m][n];
+		for(int i=0; i<m;i++) {
+			s[i][0] = 1;
+		}
+		for(int i=0; i<n;i++) {
+			s[0][i] = 1;
+		}
+		for(int i =1; i<m; i++) {
+			for(int j=1; j<n; j++) {
+				s[i][j] = s[i-1][j]+s[i][j-1];
+			}
+		}
+		return s[m-1][n-1];
+
+	}
+}
