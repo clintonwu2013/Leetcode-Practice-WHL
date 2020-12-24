44545c6117cd5181b7490fb5e4b4744cd8e58dc3
吳宗霖 <clintonwu2013@gmail.com>
2020-12-25 00:17:48 +0800

    commti leetcode

--- JavaApplication/src/leetcode20200527/Leetcode5.java
+++ JavaApplication/src/leetcode20200527/Leetcode5.java
@@ -10,7 +10,7 @@ public class Leetcode5 {
 
 		for (int i = 1; i < s.length(); i++) {
 			for (int j = 0; j < i; j++) {
-				if (s.charAt(i) == s.charAt(j)) {
+				if (s.charAt(i) == s.charAt(j)) { 
 					if (i - j == 1 || d[i - 1][j + 1]) {
 						d[i][j] = true;
 						if (end - start < i - j) {
